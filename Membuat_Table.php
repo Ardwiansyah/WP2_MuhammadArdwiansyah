f217f920584f1243ab24eaa521fc58af6723e819
Ardwiansyah <ardwiansyah230801@gmail.com>
2022-05-29 17:41:07 +0700

    create Membuat table

---
+++ Membuat_Table.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table border="1" cellpadding=5 cellspacing=3> 
+        <thead>
+            <tr>
+                <th colspan="3" class="coklat">DAFTAR PRODUK</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="kuning" rowspan="4"><img src="canon.jpg" width="200px"></td>
+            </tr>
+            <tr>
+                <td class="hijau">Nama Produk</td>
+                <td class="pink">Canon EOS M10 Kit EF-M 15-45mm</td>
+            </tr>
+            <tr>
+                <td class="hijau">Harga</td>
+                <td class="pink">Rp 4.899.000</td>
+            </tr>
+            <tr>
+                <td class="hijau">Fitur Produk</td>
+                <td class="pink">
+                    <ul>
+                        <li>Kamera mirrorless</li>
+                        <li>Efektifitas Pixel : 18 MP</li>
+                        <li>tipe Sensor : CMOS DIGIC 6</li>
+                        <li>Layar : 3.0 inch</li>
+                        <li>Memiliki lampu flash dengan jarak jangkauan hingga 5 meter</li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
